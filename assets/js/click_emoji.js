6a9f32d3d76ccbd4ff89556167f28943494cf3be
张焕明 <765979416@qq.com>
2022-02-17 14:41:48 +0800

    添加点击emoji

--- assets/js/click_emoji.js
+++ assets/js/click_emoji.js
@@ -31,8 +31,8 @@ var emj_arr = ["&#129409", "&#8986", "&#8987", "&#9200", "&#9748", "&#9749", "&#
     a.className = "heart",
     d.push({
       el: a,
-      x: e.clientX - 5,
-      y: e.clientY - 5,
+      x: e.clientX,
+      y: e.clientY,
       scale: 1,
       alpha: 1
     }),
